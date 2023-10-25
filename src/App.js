db6c1f580d4632e66d46eeddc6da9f7501377416
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-10-26 00:11:13 +0300

    learn-examples

--- src/App.js
+++ src/App.js
@@ -45,16 +45,33 @@ import styles from "./App.module.css";
 // }
 
 //! Example 2
-function App() {
-  const age = 19;
-  const isGreen = true;
+// function App() {
+//   const age = 19;
+//   const isGreen = false;
 
-  return (
-    <div className={styles.App}>
-      {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
-      <h1 style={{ color: isGreen ? "green" : "red", backgroundColor: "black" }}>THIS HAS COLOR</h1>
-    </div>
-  );
+//   return (
+//     <div className={styles.App}>
+//       {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+//       <h1 style={{ color: isGreen ? "green" : "red", backgroundColor: "black" }}>THIS HAS COLOR</h1>
+//       {isGreen && <button>THIS IS A BUTTON</button>}
+//     </div>
+//   );
+// }
+//! Example 3
+
+function App() {
+  const users = [
+    { name: "reso", age: 27 },
+    { name: "ahmet", age: 29 },
+    { name: "mehmet", age: 35 },
+  ];
+
+  return <div className={styles.App}><h1>
+    {users.map((user, key) => {
+      return <div>{user.name} {user.age}</div>
+    })}
+
+  </h1></div>
 }
 
 
