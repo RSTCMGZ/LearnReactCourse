a0d6831876e6b6ea01e219f9f993e48948f7ac37
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-10-30 21:19:23 +0300

    project-finish

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
+import App from './App';
 // import Planets from "./planets"
-import Excercise from "./Excercise"
+// import Excercise from "./Excercise"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Excercise />
+    <App />
+    {/* <Excercise /> */}
     {/* <Planets /> */}
   </React.StrictMode>
 );
